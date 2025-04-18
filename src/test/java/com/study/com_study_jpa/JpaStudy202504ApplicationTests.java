28e4bc22cfb847af114dbfdabf3b1da98b40fba7
hyeonguk <shyunguk126@naver.com>
2025-04-18 15:06:19 +0900

    initial commit

---
+++ src/test/java/com/study/com_study_jpa/JpaStudy202504ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.com_study_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaStudy202504ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
